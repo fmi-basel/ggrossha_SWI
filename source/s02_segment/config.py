11917e428e099a5ba38a5c63bcc365d589556e14
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2025-03-26 14:50:50 +0100

    Add prepare annotation data scripts.

--- source/s02_segment/config.py
+++ source/s02_segment/config.py
@@ -76,3 +76,71 @@ class SegmentationConfig(IPAConfig):
 
         config.save()
         return config
+
+
+class PrepareTrainingDataConfig(IPAConfig):
+    zarr_data_dir: Path
+    output_dir: Path
+    base_name: str
+    positions: list[int]
+    brightfield_channel_index: int
+
+    @staticmethod
+    def config_name() -> str:
+        return "prepare_training_data_config.yaml"
+
+    @classmethod
+    def prompt(cls) -> "PrepareTrainingDataConfig":
+        try:
+            loaded_config = cls.load()
+        except FileNotFoundError:
+            loaded_config = cls(
+                zarr_data_dir=get_git_root() / "processed_data",
+                output_dir=get_git_root() / "processed_data",
+                base_name="worm_s",
+                positions=[1],
+                brightfield_channel_index=0,
+            )
+
+        zarr_data_dir = questionary.path(
+            "[prepare_training_data]: Path to zarr data directory:",
+            default=str(loaded_config.zarr_data_dir),
+        ).ask()
+        output_dir = questionary.path(
+            "[prepare_training_data]: Path to output directory:",
+            default=str(loaded_config.output_dir).replace("prepare_training_data", ""),
+        ).ask()
+        base_name = questionary.text(
+            "[prepare_training_data]: Base name of the datasets:",
+            default=loaded_config.base_name,
+        ).ask()
+        positions = [
+            int(c)
+            for c in questionary.text(
+                "[prepare_training_data]: List positions to process (comma separated):",
+                validate=lambda x: x.replace(",", "").isdigit(),
+                default=",".join([str(c) for c in loaded_config.positions]),
+            )
+            .ask()
+            .split(",")
+        ]
+        brightfield_channel_index = int(
+            questionary.text(
+                "[prepare_training_data]: Brightfield channel index (zero-indexed):",
+                validate=lambda x: x.isdigit() and int(x) >= 0,
+                default=str(loaded_config.brightfield_channel_index),
+            ).ask()
+        )
+        output_dir = Path(output_dir) / "annotation_data"
+        output_dir.mkdir(exist_ok=True)
+
+        config = PrepareTrainingDataConfig(
+            zarr_data_dir=Path(zarr_data_dir),
+            output_dir=output_dir,
+            base_name=base_name,
+            positions=positions,
+            brightfield_channel_index=brightfield_channel_index,
+        )
+
+        config.save()
+        return config
---
+++ source/s02_segment/prepare_annotation_data.py
@@ -0,0 +1,67 @@
+from os.path import basename, join
+
+import numpy as np
+import zarr
+from ome_zarr.io import parse_url
+from tifffile import imwrite
+from tqdm import tqdm
+from micro_sam.precompute_state import precompute_state
+from rich.pretty import pretty_repr
+
+from faim_ipa.utils import create_logger
+import sys
+from faim_ipa.utils import get_git_root
+
+sys.path.append(str(get_git_root()))
+from source.s02_segment.config import PrepareTrainingDataConfig
+
+
+def main(
+    config: PrepareTrainingDataConfig,
+):
+    logger = create_logger("prepare-annotation-data")
+    logger.info("Config:")
+    logger.info(pretty_repr(config))
+
+    zarr_dirs = list(config.zarr_data_dir.glob("*.zarr"))
+    zarr_dirs = list(
+        filter(
+            lambda x: int(
+                basename(x).replace(config.base_name, "").replace(".zarr", "")
+            )
+            in config.positions,
+            zarr_dirs,
+        )
+    )
+
+    logger.info(f"Found {len(zarr_dirs)} zarr files.")
+
+    for zarr_dir in tqdm(zarr_dirs):
+        logger.info(f"Processing {zarr_dir}")
+        data = zarr.Group(parse_url(zarr_dir, mode="r").store)[0]
+
+        logger.info("Extract raw data projections...")
+        annotation_planes = []
+        for i in tqdm(range(data.shape[0]), leave=False):
+            stack = data[i, config.brightfield_channel]
+            idx = max(6, np.argmax(np.std(stack, axis=(1, 2))))
+            annotation_planes.append(stack[idx - 6 : idx + 1].mean(0).astype(np.uint16))
+
+        annotation_planes = np.stack(annotation_planes)
+        out_path = join(
+            config.output_dir, f"{basename(zarr_dir).replace('.zarr', '.tif')}"
+        )
+        imwrite(out_path, annotation_planes)
+
+        logger.info("Precomputing SAM embeddings...")
+        precompute_state(
+            input_path=out_path,
+            output_path=out_path.replace(".tif", "-embedding.zarr"),
+        )
+
+    logger.info("Done.")
+
+
+if __name__ == "__main__":
+    config = PrepareTrainingDataConfig.load()
+    main(config)
