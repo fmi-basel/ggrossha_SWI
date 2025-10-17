dd7da7739cea0b58efbc0679eda6ac4e4e7c25c4
Jan Eglinger <jan.eglinger@fmi.ch>
2025-10-17 15:38:37 +0200

    Re-add gres=gpu

--- source/s02_segment/slurm-job-train-network.sh
+++ source/s02_segment/slurm-job-train-network.sh
@@ -7,6 +7,7 @@
 #SBATCH --output=slurm_output/run-%j.out
 #SBATCH --error=slurm_output/run-%j.err
 #SBATCH --partition=main
+#SBATCH --gres=gpu:1
 #SBATCH --mem=196GB
 #SBATCH --constraint "infiniband&gpuram80gb"
 #SBATCH --time=48:00:00
