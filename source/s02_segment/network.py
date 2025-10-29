ec89f8b735cc31c07cc3d8d9ad0df95a4ab09639
Jan Eglinger <jan.eglinger@fmi.ch>
2025-10-29 23:04:11 +0100

    network.py: restore key_separator

--- source/s02_segment/network.py
+++ source/s02_segment/network.py
@@ -29,7 +29,7 @@ class WormDataset(Dataset):
         shuffle: bool = True,
     ):
         store = parse_url(zarr_file, mode="r").store
-        # store.key_separator = "."
+        store.key_separator = "."
         self.zarr_x = zarr.group(store)["0"]
         self.zarr_y = zarr.group(store)["labels"]["mask"]["0"]
         self.patches = self.select_patches(
