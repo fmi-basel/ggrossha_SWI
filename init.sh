557e28abfcd8547d5fcb66f7bef256928e54925f
Jan Eglinger <jan.eglinger@fmi.ch>
2025-10-30 16:26:46 +0100

    Clean up init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 # Initialize pixi and set cache and temporary directories.
 WD="$(realpath .)"
-export PATH=$PATH:"$WD/infrastructure/apps/pixi/bin"
+export PATH="$WD/infrastructure/apps/pixi/bin":$PATH
 export PIXI_CACHE_DIR="$WD/infrastructure/apps/pixi/.pixi_cache"
 export TMPDIR="/tmp/.tmp_$USER"
 mkdir -p "$TMPDIR"
 
 mkdir -p "$WD/slurm_output"
-
-micro_sam="$HOME/.cache/micro_sam/"
-if [ ! -d "$micro_sam" ]; then
-    echo "Please create the ~/.cache/micro_sam/ directory."
-    exit 1
-fi
 
 # Single threaded
 export OMP_NUM_THREADS=1
@@ -24,4 +18,4 @@ export NUMEXPR_NUM_THREADS=1
 export DASK_DISTRIBUTED__SCHEDULER__WORK_STEALING="False"
 
 # Ensure that the latest documentation is built.
-pixi run build_docs
+pixi self-update --version 0.48.0
